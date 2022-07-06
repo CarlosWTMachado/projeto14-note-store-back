54f33a921a85c0663be2767f0eb0eb97a4e38372
Carlos William <carloswtmachado@gmail.com>
2022-07-06 17:48:50 -0300

    arquivo de conexao com db

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Commit incial")
+import express, { json } from 'express';
+import cors from 'cors';
+//import TransactionRouter from './routes/transactionRouter.js';
+
+const server = express();
+server.use(cors());
+server.use(json());
+
+//server.use(TransactionRouter);
+
+server.listen(process.env.PORT, () => {
+	console.log("Rodando ...");
+});
