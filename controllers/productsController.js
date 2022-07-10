40f4fa7241b4874d920f8e729e15248b4b9523bf
Carlos William <carloswtmachado@gmail.com>
2022-07-09 23:27:34 -0300

    rotas carrinho

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,16 +1,52 @@
 import db from '../db/db.js';
+import {ObjectId} from 'mongodb';
+import {v4 as uuid} from 'uuid';
 
 export async function RetornaProdutos(req, res) {
+	const {carrinho} = req.headers;
 	try {
 		const produtos = await db.collection("produtos").find({}).toArray();
-		res.send(produtos);
+		const token = (carrinho) ? carrinho : uuid();
+		res.send({produtos, token});
+	} catch (error) {
+		return res.status(500).send(error);
+	}
+}
+
+export async function AdicionaCarrinho(req, res){
+	const {carrinho} = req.headers;
+	const {id} = req.body;
+	try {
+		const produto = await db.collection("produtos").findOne({_id: new ObjectId(id)});
+		await db.collection("carrinho").insertOne({
+			carrinhoId: carrinho,
+			produtoId: produto._id,
+			quatidade: 1
+		});
+		res.sendStatus(200);
+	} catch (error) {
+		return res.status(500).send(error);
+	}
+}
+
+export async function RetornaCarrinho(req, res){
+	const {carrinho} = req.headers;
+	try {
+		const cart = await db.collection("carrinho").find({carrinhoId: carrinho}).toArray();
+		res.send(cart);
 	} catch (error) {
 		return res.status(500).send(error);
 	}
 }
 
 /*
-imagem:
-titulo:
-preco:
+_id
+titulo
+imagem
+preco
+
+_id
+usuarioId
+produtoId
+quantidade
 */
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {RetornaProdutos} from '../controllers/productsController.js';
+import {RetornaProdutos, AdicionaCarrinho, RetornaCarrinho} from '../controllers/productsController.js';
 //import {  } from '../middlewares/productsMiddleware.js';
 
 const router = express.Router();
 //router.post('/produtos', );
 router.get('/produtos', RetornaProdutos);
+router.get('/carrinho', RetornaCarrinho);
+router.post('/carrinho', AdicionaCarrinho);
 export default router;
