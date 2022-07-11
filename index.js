7181c3c7bb272993bd50cbb02216b2d68f7ae799
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-07-11 18:34:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import dotenv from "dotenv";
 import ProductsRouter from './routes/productsRouter.js';
+import userRouter from './routes/userRouter.js';
+
+dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(json());
 
 server.use(ProductsRouter);
+server.use(userRouter);
 
 server.listen(process.env.PORT, () => {
 	console.log("Rodando ...");
